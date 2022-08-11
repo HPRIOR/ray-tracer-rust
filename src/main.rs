d489b21b3993544b66f2a6dacb58f8d0c2614f96
Harry Prior <harryprior@hotmail.co.uk>
2022-08-11 19:02:58 +0100

    add clock challenge

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+#![allow(dead_code)]
+use std::f64::consts::PI;
+
 use module_lib::{
     canvas::canvas::Canvas,
     colour::colour::Colour,
-    projectile_test::projectile::{tick, Env, Projectile}, geometry::vector::{Vector, Operations},
+    geometry::vector::{point, Operations, Vector},
+    matrix::matrix::{Axis, Matrix},
+    projectile_test::projectile::{tick, Env, Projectile},
 };
 
-fn main() {
+fn create_projectile_canvas(file_name: &str) {
     let canvas_height = 500;
     let canvas_width = 1000;
     let mut canvas = Canvas::new(canvas_width, canvas_height);
@@ -35,6 +40,74 @@ fn main() {
     }
 
     println!("saving canvas");
-    canvas.save("/Users/harryprior/Code/ray-tracer/resources/test.ppm");
+    canvas.save(
+        format!(
+            "/home/harry/Code/ray-tracer-rust/resources/{}.ppm",
+            file_name
+        )
+        .as_str(),
+    );
+}
 
+fn create_clock() {
+    let canvas_height = 500;
+    let canvas_width = 500;
+
+    let mut canvas = Canvas::new(canvas_width, canvas_height);
+    let center = Matrix::ident()
+        .translate(250.0, 250.0, 0.0)
+        .mul_tup(point(0.0, 0.0, 0.0));
+
+    let length = 100.0;
+    let center_coord = Coord {
+        x: center.0,
+        y: center.1,
+    };
+    set_pixel(center_coord, &mut canvas);
+
+    (0..12).map(|i| i as f64 * 30.0 + 15.0).for_each(|degree| {
+        set_pixel(get_point(degrees_to_radians(degree), length), &mut canvas);
+    });
+
+    canvas.save(
+        format!(
+            "/home/harry/Code/ray-tracer-rust/resources/{}.ppm",
+            "test_clock"
+        )
+        .as_str(),
+    )
+}
+
+struct Coord {
+    x: f64,
+    y: f64,
+}
+
+fn get_point(radians: f64, length: f64) -> Coord {
+    let coords = Matrix::ident()
+        .translate(length, length, 0.0)
+        .rotate(Axis::Z, radians)
+        .translate(250.0, 250.0, 0.0)
+        .mul_tup(point(0.0, 0.0, 0.0));
+    Coord {
+        x: coords.0,
+        y: coords.1,
+    }
+}
+
+fn set_pixel(coord: Coord, canvas: &mut Canvas) {
+    canvas.set_pixel(
+        coord.x as usize,
+        coord.y as usize,
+        Colour::new(1.0, 1.0, 1.0),
+    );
+}
+
+fn degrees_to_radians(degrees: f64) -> f64 {
+    (PI / 180.0) * degrees
+}
+
+fn main() {
+    // create_projectile_canvas("projectile");
+    create_clock();
 }
