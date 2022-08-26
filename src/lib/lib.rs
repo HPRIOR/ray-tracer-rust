6a4a40b4e189a3fc851ea6ffcbf464d65f1396bf
Harry Prior <harryprior@hotmail.co.uk>
2022-08-26 11:14:15 +0100

    add exercises to module structure

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,11 +1,11 @@
 pub mod geometry;
-pub mod projectile_test;
 pub mod colour;
 pub mod canvas;
 pub mod matrix;
 pub mod utils;
 pub mod ray;
 pub mod shapes;
+pub mod exercises;
 
 pub fn lib_entry(){
     
