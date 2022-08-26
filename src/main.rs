fab6d0176351db7175989e48c1d9c3194536efc0
Harry Prior <harryprior@hotmail.co.uk>
2022-08-26 11:16:15 +0100

    format

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![allow(dead_code, unused_imports)]
 
-use module_lib::exercises::{projectile_test::projectile::create_projectile_canvas, clock::clock::create_clock, ray_sphere::ray_sphere::render_sphere};
+use module_lib::exercises::{
+    clock::clock::create_clock, projectile_test::projectile::create_projectile_canvas,
+    ray_sphere::ray_sphere::render_sphere,
+};
 
 fn main() {
     //create_projectile_canvas("projectile");
