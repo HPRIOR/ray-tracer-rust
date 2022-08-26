1843100fd161de052f20d2cc7c3e3061e67c86a4
Harry Prior <harryprior@hotmail.co.uk>
2022-08-26 12:11:21 +0100

    use par_iter for some insane speedups

--- src/lib/exercises/ray_sphere/ray_sphere.rs
+++ src/lib/exercises/ray_sphere/ray_sphere.rs
@@ -1,8 +1,6 @@
-#![allow(unused_imports, unused_variables, dead_code, unused_mut)]
-
 use crate::{
     canvas::canvas::Canvas,
-    exercises::shared::shared::{set_pixel, Coord, save_canvas},
+    exercises::shared::shared::{save_canvas, set_pixel, Coord},
     geometry::vector::{point, vector},
     matrix::matrix::Matrix,
     ray::{
@@ -11,10 +9,12 @@ use crate::{
     },
     shapes::sphere::Sphere,
 };
+use rayon::prelude::*;
 
 pub fn render_sphere() {
-    let sphere = Sphere::with_transform(Matrix::scaling(10.0, 10.0, 10.0));
-    let (width, height) = (100, 100);
+    let sphere =
+        Sphere::with_transform(Matrix::scaling(400.0, 400.0, 400.0).translate(500.0, 500.0, 0.0));
+    let (width, height) = (1000, 1000);
 
     let mut canvas = Canvas::new(width, height);
 
@@ -25,18 +25,26 @@ pub fn render_sphere() {
         })
         .collect();
 
-    for ray in rays {
-        let intersections: Option<Vec<Intersection>> = ray.intersect(&sphere);
 
-        let hit = intersections.hit();
+    let coords: Vec<Coord> = rays
+        .par_iter()
+        .filter_map(|ray| {
+            let intersections: Option<Vec<Intersection>> = ray.intersect(&sphere);
+            let hit = intersections.hit();
+            if let Some(hit) = hit {
+                let p = ray.position(hit.at);
+                Some(Coord { x: p.0, y: p.1 })
+            } else {
+                None
+            }
+        })
+        .collect();
 
-        if let Some(hit) = hit {
-            let p = ray.position(hit.at);
-            set_pixel(Coord { x: p.0, y: p.1 }, &mut canvas);
-        }
-    }
+    coords.into_iter().for_each(|coord| {
+        set_pixel(coord, &mut canvas);
+    });
+
     save_canvas("sphere_test", &canvas);
-
 }
 
 #[cfg(test)]
